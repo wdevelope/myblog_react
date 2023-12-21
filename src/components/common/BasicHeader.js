94b8826864fe6d1ecb091c98e39b24fa80bbae9e
seongwong woo <dnqkr18@gmail.com>
2023-12-21 15:07:15 +0900

    feat: 로그인시 버튼 동적변화

--- src/components/common/BasicHeader.js
+++ src/components/common/BasicHeader.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './BasicHeader.module.css';
-
+import { useNavigate } from 'react-router-dom';
 export default function BasicHeader() {
+  const navigate = useNavigate();
+  const isLoggedin = document.cookie.includes('Authorization');
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    // 쿠키에서 Authorization 제거
+    document.cookie = 'Authorization=; Max-Age=0';
+    alert('로그아웃에 성공했습니다.');
+    navigate('/home');
+  };
+
   return (
     <div className={styles.header}>
       <Link to="/home">
         <h2>w-life blog</h2>
       </Link>
       <div className={styles.LoginRegisterDiv}>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {isLoggedin ? (
+          <>
+            <Link to="#" onClick={handleLogout}>
+              Logout
+            </Link>
+            <Link to="/account">Account</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </div>
     </div>
   );

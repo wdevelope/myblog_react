11c484776b5c2623a316b9faad59709357396cc3
seongwong woo <dnqkr18@gmail.com>
2024-01-13 09:08:26 +0900

    css: 코드블록 폰트사이즈 조정

--- src/components/common/CustomReactQuill.js
+++ src/components/common/CustomReactQuill.js
@@ -8,7 +8,7 @@ const CustomReactQuill = ({ value, onChange, readOnly = false }) => {
     toolbar: readOnly
       ? false
       : [
-          [{ header: 1 }, { header: 2 }], // 헤더 크기
+          [{ header: [1, 2, 3, false] }], // 헤더 크기
           ['bold', 'italic', 'underline', 'strike'], // 표준 텍스트 포맷팅
           ['blockquote', 'code-block'], // 블록 인용문 및 코드 블록
 
@@ -16,13 +16,13 @@ const CustomReactQuill = ({ value, onChange, readOnly = false }) => {
           [{ indent: '-1' }, { indent: '+1' }], // 들여쓰기
 
           [{ color: [] }, { background: [] }], // 글자 색상 및 배경색
-          [{ font: [] }], // 폰트 스타일
           [{ align: [] }], // 정렬
 
           ['clean'], // 포매팅 제거
         ],
-
-    // ... 기타 모듈 설정
+    clipboard: {
+      matchVisual: true,
+    },
   };
 
   return <ReactQuill value={value} onChange={onChange} modules={modules} theme="snow" readOnly={readOnly} />;
